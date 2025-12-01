eea371b90169edf542b499f384cacb7ce73cc927
CDProjects <caseydentprojects@gmail.com>
2025-12-01 13:19:23 +0200

    react-leaflet, axios installed. stationData.ts created

--- frontend/web/utils/api.ts
+++ frontend/web/utils/api.ts
@@ -1,5 +1,7 @@
 // frontend/web/utils/api.ts
 import axios from 'axios';
+import 'leaflet/dist/leaflet.css';
+
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api', // Your backend server
